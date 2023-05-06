0d369bcc34309d958d8d9295cdbed0a97ca09e52
yilizzz <yilizhang3@gmail.com>
2023-05-06 11:45:44 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>
